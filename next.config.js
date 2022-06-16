7127a8b0727cf9ee770c18571c3381221bfabe64
Reiga <remrem4862@gmail.com>
2022-06-16 19:24:13 +0900

    Update Readme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
+  eslint: {
+    dirs: ['pages/', 'components/']
+  }
 }
 
 module.exports = nextConfig
