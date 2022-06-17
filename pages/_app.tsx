a50d027c59fedab9a16536e6e3905c90ec9369e8
Reiga <remrem4862@gmail.com>
2022-06-17 22:49:32 +0900

    updated v2 and add Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function app({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>NationalFocus Viewer</title>
+                <meta name="description" content="support your modding."/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
-export default MyApp
+export default app
