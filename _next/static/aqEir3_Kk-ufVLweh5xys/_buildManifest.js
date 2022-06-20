c3db3fc1cae833294c9e9b78c376b5f37e3f7876
Ign1s-Reiga <Ign1s-Reiga@users.noreply.github.com>
2022-06-20 13:21:46 +0000

    deploy: 059e6ab1554760d8f91379624f4516ba9cd86b79

---
+++ _next/static/aqEir3_Kk-ufVLweh5xys/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/666-6e9084c49b93dc21.js","static/css/bc34ef27239f9c74.css","static/chunks/pages/index-680ee5adbebe4f95.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
