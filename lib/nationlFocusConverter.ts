c637d15eceedd63e2e37ca98d6b566d5c79f8749
Reiga <remrem4862@gmail.com>
2022-06-20 19:44:39 +0900

    deploy.yml

---
+++ lib/nationlFocusConverter.ts
@@ -0,0 +1,37 @@
+export interface INationalFocusTree {
+    id: string
+    focuses: INationalFocus[]
+}
+export interface INationalFocus {
+    id: string
+    icon: string
+    cost: number
+    x: number
+    y: number
+    relativePositionId: INationalFocus | null
+    prerequisite: INationalFocus[] | null
+    mutuallyExclusive: INationalFocus[] | null
+    searchFilters: ''
+    aiWillDo: {factor: number, modifier: {}}
+    completeReward: string
+    completeTooltip: string
+}
+export const searchFilters = {
+    POLITICAL: 'FOCUS_FILTER_POLITICAL',
+    RESEARCH: 'FOCUS_FILTER_RESEARCH',
+    INDUSTRY: 'FOCUS_FILTER_INDUSTRY',
+    STABILITY: 'FOCUS_FILTER_STABILITY',
+    WAR_SUPPORT: 'FOCUS_FILTER_WAR_SUPPORT',
+    MANPOWER: 'FOCUS_FILTER_MANPOWER',
+    ANNEXATION: 'FOCUS_FILTER_ANNEXATION',
+    SOV_POLITICAL_PARANOIA: 'FOCUS_FILTER_SOV_POLITICAL_PARANOIA',
+    PROPAGANDA: 'FOCUS_FILTER_PROPAGANDA',
+    SPA_CIVIL_WAR: 'FOCUS_SPA_FILTER_CIVIL_WAR',
+    SPA_CARLIST_UPRISING: 'FOCUS_FILTER_SPA_CARLIST_UPRISING',
+    ARMY_XP: 'FOCUS_FILTER_ARMY_XP',
+    NAVY_XP: 'FOCUS_FILTER_NAVY_XP',
+    AIR_XP: 'FOCUS_FILTER_AIR_XP'
+}
+export class NFConverter {
+
+}
