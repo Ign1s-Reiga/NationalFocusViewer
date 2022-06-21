9333408ad42f3bceb9281cb452a9cf360421278a
Ign1s_Reiga <64723970+Ign1s-Reiga@users.noreply.github.com>
2022-06-21 19:42:21 +0900

    feature: Add Context Menu (#5)
    * add ContextMenu & NF UI
    
    * add coment

--- components/NFContainer.tsx
+++ components/NFContainer.tsx
@@ -1,10 +1,86 @@
 import styles from '../styles/NFContainer.module.css'
+import { INationalFocus } from '../lib/nationlFocusConverter';
+import React from 'react';
+import { Menu, MenuItem } from '@mui/material';
+import NationalFocus from './NationalFocus';
+
+const nfs: INationalFocus[] = [
+    {
+        id: 'てすと1',
+        icon: null,
+        cost: 7,
+        x: 5,
+        y: 2,
+        relativePositionId: null,
+        prerequisite: null,
+        mutuallyExclusive: null,
+        searchFilters: [],
+        aiWillDo: {factor: 1, modifier: {}},
+        completeReward: '',
+        completeTooltip: '',
+    },
+    {
+        id: 'てすと2',
+        icon: null,
+        cost: 7,
+        x: -5,
+        y: 2,
+        relativePositionId: null,
+        prerequisite: null,
+        mutuallyExclusive: null,
+        searchFilters: [],
+        aiWillDo: {factor: 1, modifier: {}},
+        completeReward: '',
+        completeTooltip: '',
+    }
+]
 
 const NFContainer = (props: {selectedItem: string}) => {
+    const [contextMenu, setContextMenu] = React.useState<{
+        mouseX: number;
+        mouseY: number;
+    } | null>(null);
+
+    const handleContextMenu = (event: React.MouseEvent) => {
+        event.preventDefault();
+        setContextMenu(
+            contextMenu === null
+                ? {
+                    mouseX: event.clientX + 2,
+                    mouseY: event.clientY - 6,
+                } : null,
+        );
+    };
+
+    const handleKeyUp = (event: React.KeyboardEvent) => {
+        event.preventDefault();
+        if ('Escape' === event.key)
+            handleClose();
+    };
+
+    const handleClose = () => {
+        setContextMenu(null);
+    };
+
     return (
-        <div className={styles.nfContainer}>
+        <div id={'nfContainer'} className={styles.nfContainer} onContextMenu={handleContextMenu} onKeyUp={handleKeyUp}>
             <img src={'/tiled_bg.png'} alt={''} className={styles.nfContainerBg}/>
             <img src={'/tiled_focus_bg.png'} alt={''} className={styles.nfContainerFocusBg}/>
+            {nfs.map(v => <NationalFocus nfData={v} key={v.id}/>)}
+            <Menu
+                open={contextMenu !== null}
+                onClose={handleClose}
+                anchorReference="anchorPosition"
+                anchorPosition={
+                    contextMenu !== null
+                        ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+                        : undefined
+                }
+                PaperProps={{sx: {bgcolor: '#202020', color: 'white'}}}
+            >
+                <MenuItem onClick={handleClose}>コピー</MenuItem>
+                <MenuItem onClick={handleClose}>ペースト</MenuItem>
+            </Menu>
         </div>
     )
 }
